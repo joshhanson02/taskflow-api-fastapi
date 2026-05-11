c12c6689734bdd3a438a779d82c61999334dd655
joshchan1301 <trunghieuidol02@gmail.com>
2026-05-11 08:36:11 +0700

    fix: add min_length for password, update requirements.txt and add alembic to change nullable = False with username, email and password

--- schemas/auth_schema.py
+++ schemas/auth_schema.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from pydantic import EmailStr  # Dùng để xác thực form email
 
 
 class RegisterRequest(BaseModel):
     username: str
     email: EmailStr
-    password: str
+    password: str = Field(..., min_length=6)
 # Create class to validate data input
 
 
 class LoginRequest(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(..., min_length=6)
