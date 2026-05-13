bb3e3f28e7666e1708971ceae34bfb752d595b64
joshchan1301 <trunghieuidol02@gmail.com>
2026-05-13 15:36:52 +0700

    refactor database initialization into application lifecycle and remove standalone init script

--- db/database.py
+++ db/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 from core.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
--- db/init_db.py
+++
@@ -1,4 +0,0 @@
-from db.database import engine, Base
-from models.user_model import User
-
-Base.metadata.create_all(bind=engine)
