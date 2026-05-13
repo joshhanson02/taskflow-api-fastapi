85f387314a36fa38ec0b3755cac9952ae0078374
joshchan1301 <trunghieuidol02@gmail.com>
2026-05-13 15:38:02 +0700

    integrate task module with database initialization and API routing

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
-from controllers import auth_controller
-from controllers import admin_controller
+from controllers import auth_controller, admin_controller, task_controller
 from exceptions.auth_exception import AuthException
 from handlers.exception_handler import auth_exception_handler
+from db.database import Base, engine
+from models.user_model import User
+from models.task_model import Task
 app = FastAPI()
-
+Base.metadata.create_all(bind=engine)
 app.include_router(
     auth_controller.router
 )
 
 app.include_router(
     admin_controller.router
+)
+
+app.include_router(
+    task_controller.router
 )
 
 app.add_exception_handler(
